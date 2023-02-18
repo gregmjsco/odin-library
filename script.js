f0c754d94f232f272faef10b6f6e739e391e3d71
Greg Johnston <gregmj.sco@gmail.com>
2023-02-18 18:08:40 +0900

    Add book boilerplate function

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function(){
+    let info = `${title} by ${author}, ${pages}, ${read}`
+    return info
+  }
+
 }
 
 function addBookToLibrary() {
