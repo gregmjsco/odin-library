98b075586d4ade000fbedcd50d10294b0d91f1c1
Greg Johnston <gregmj.sco@gmail.com>
2023-03-04 12:13:16 +0900

    Add event to form that calls addBookToLibrary

--- script.js
+++ script.js
@@ -10,15 +10,18 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function(){
-    var info = `${title} by ${author}, ${pages}, ${read}`
-    return info;
   }
-
-}
 
 function addBookToLibrary() {
   // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+
+  let newBook = new Book(title, author, pages, read);
+  console.log(newBook);
+
 }
 
 let newBookBtn = document.querySelector("#add-book-btn");
@@ -26,4 +29,9 @@ newBookBtn.addEventListener("click", function(){
   let newBookForm = document.querySelector("#new-book-form");
   console.log(newBookForm);
   newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function(event) {
+  event.preventDefault()
+  addBookToLibrary()
 })
